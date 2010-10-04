48bfd0e9c5fd4da96974393ba446fc4a3f8b087b
Leonardo Santagada <santagada@gmail.com>
2010-10-04 12:12:41 -0300

    added zenburn theme (from http://bitbucket.org/kcfelix/zenburn-theme.el) and made it default

--- appearance.el
+++ appearance.el
@@ -8,11 +8,11 @@
 
 ;; Set default colors to green on black
 ;; will override this with theme below, if you want
-(setq default-frame-alist
-      '(
-        (background-color . "black")
-        (foreground-color . "green")
-        ))
+;; (setq default-frame-alist
+;;       '(
+;;         (background-color . "black")
+;;         (foreground-color . "green")
+;;         ))
 
 ;; set up color theme of choice by uncommenting the one you want here
 ;; or simply using one of Aquamacs included themes
@@ -26,5 +26,6 @@
 ; (color-theme-twilight)
 ; (load-file (concat kitfiles-dir "/vendor" "/blackboard.el"))
 ; (color-theme-blackboard)
+(load-theme 'zenburn)
 
 (provide 'appearance)
